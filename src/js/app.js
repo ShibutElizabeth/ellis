76aaa27c5101fe606ca3ecfb3260061b16e45324
ShibutElizabeth <ellis.shybut@mail.ru>
2023-08-07 18:52:22 +0200

    test: new

--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,9 @@ import { Hero } from "./animations/Hero";
 import { Contact } from "./animations/Contact";
 import { initScroll } from "./LocoScroll";
 import { Projects } from "./animations/Projects";
-import { Preloader } from "./Stories/Preloader.class";
 import { Cursor } from "./animations/Cursor";
 
 window.addEventListener('load', () => {
-    // const preloader = new Preloader();
     initScroll();
     const cursor = new Cursor();
     const hero = new Hero();
--- src/shaders/fragment-blob.glsl
+++
@@ -1,7 +0,0 @@
-uniform float time;
-uniform vec3 uColor;
-varying float vNoise;
-
-void main(){
-    gl_FragColor = (1. - vNoise) * vec4(uColor, 0.6);
-}
